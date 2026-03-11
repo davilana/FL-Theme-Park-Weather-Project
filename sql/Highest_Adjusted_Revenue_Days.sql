0c7de75fc753d579d5f329f951b8fa05ff705404
Natasha Davila <natasha.davila7@gmail.com>
2026-03-10 23:07:14 -0400

    Create table for highest adjusted revenue days

---
+++ sql/Highest_Adjusted_Revenue_Days.sql
@@ -0,0 +1,17 @@
+CREATE TABLE "Highest Adjusted Revenue Days" AS
+SELECT
+Date,
+Park_Name,
+Adjusted_Revenue
+FROM (
+    SELECT
+        Date, 
+        Park_Name,
+        Adjusted_Revenue,
+        ROW_NUMBER() OVER (
+            PARTITION BY Park_Name
+            ORDER BY Adjusted_Revenue DESC
+        ) AS rank
+    FROM Adjusted_Model
+)    
+WHERE rank = 1;
